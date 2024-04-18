401347fa13ca6cf22a68266ba64242e80049356a
Ramesh KHARAT <62638281+kharatramesh@users.noreply.github.com>
2024-04-18 09:38:56 +0530

    Update remotebackend.tf

--- terraform/remotebackend.tf
+++ terraform/remotebackend.tf
@@ -3,6 +3,13 @@ provider "aws" {
    access_key = var.access_key
    secret_key = var.secret_key
 }
+resource "aws_s3_bucket" "bucket1" {
+  bucket = "bucket1232343"
+  versioning {
+    enabled=true
+     }
+
+}
 
 resource "aws_dynamodb_table" "state_locking" {
   hash_key = "LockID"
